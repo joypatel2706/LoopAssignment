2ede036d531193e75954a8c7d95ae7dc33e0509b
Jay_Patel <jaypatel78290@gmail.com>
2021-04-10 18:17:32 +0530

    Revers Digit done

--- src/com/JayPatel/Main.java
+++ src/com/JayPatel/Main.java
@@ -7,13 +7,15 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         // 1: Write program to find factorial of any number user input
-        Scanner sc= new Scanner(System.in);
+       /* Scanner sc= new Scanner(System.in);
         System.out.println("Enter number to find factorial:");
         int N = sc.nextInt();
             Factorial f = new Factorial();
             System.out.println(f.factorial(N));
-
+*/
         // 2: Write Program to revers digits
+            ReversDigits redigits = new ReversDigits();
+            redigits.reversDigits();
 
         // 3: Write program to get set of int and output sum of odd and even
     }
--- src/com/JayPatel/ReversDigits.java
+++ src/com/JayPatel/ReversDigits.java
@@ -1,5 +1,22 @@
 package com.JayPatel;
 
+import java.util.Scanner;
+
 public class ReversDigits {
+
+    public void reversDigits(){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number to revers: ");
+        long num = sc.nextLong();
+        long revNumber =0 ;
+        long tmp;
+
+        while (num > 0){
+            tmp = num % 10 ;
+            revNumber = revNumber *10 + tmp;
+            num /=  10;
+        }
+        System.out.print(revNumber);
+    }
 
 }
